f79d336e733b0002f009eff4065ee5a428017834
Young Mun Chae <dudans1536@nate.com>
2021-01-02 15:36:56 +0900

    add youngmun f.

--- output/IfdefDebug/IfdefDebug/소스.cpp
+++ output/IfdefDebug/IfdefDebug/소스.cpp
@@ -2,10 +2,17 @@
 
 using namespace std;
 
+void youngmun();
+
 int main()
 {
 #ifdef _DEBUG
 	cout << "Debug" << endl;
 #endif
 	cout << "¾È µÇ³×" << endl;
+}
+
+void youngmun()
+{
+	cout << "youngmun" << endl;
 }
