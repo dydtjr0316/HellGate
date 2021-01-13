ebb58d20c58876e065b31088c3a60e3bcaaf8009
KimHyoRim <erpppo@naver.com>
2021-01-14 01:31:41 +0900

    revert

--- output/BeAnyone/01 Project/Engine/framework.h
+++ output/BeAnyone/01 Project/Engine/framework.h
@@ -1,12 +1,3 @@
 ﻿#pragma once
 
-#include "targetver.h"
-
 #define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
-
-
-
-// 여기서 프로그램에 필요한 추가 헤더를 참조합니다.
-#include <Windows.h>
-
-#include "global.h"
